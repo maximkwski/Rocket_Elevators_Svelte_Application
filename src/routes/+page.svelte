a0855075c117bd9cab176187f742e2b636a37958
maximkwski <makkie.makowski@gmail.com>
2022-08-25 22:00:26 -0400

    home page info added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,9 @@
 <svelte:head>
     <title>Svelte Kit NFT</title>
 </svelte:head>
-<h1 class="text-4xl text-center my-8 uppercase">Hello, World!</h1>
-<p>test test test</p>
-<button>Hello!</button>
+<p>NFT is an abbreviation for the term "Non-Fungible-Token" and is essentially any piece of digital art that has a unique signature, 
+    numerical value, or designation on a blockchain or database. 
+    The reason they are "non-fungible" is that no two people can possess the same piece of art with the same designation. 
+    Go to <a href="/minting" class="text-red-400">MINTING PAGE</a> to connect your wallet. Diverse your portfolio with some of our NFTs from our collection. </p>
+<h1 class="text-4xl text-center my-8 uppercase">Our NFT Collection</h1>
+
--- src/routes/minting/+page.svelte
+++ src/routes/minting/+page.svelte
@@ -3,10 +3,10 @@
     import { writable } from "svelte/store";
 
     export const wallet_address = writable('');
-    export const freeNFTresp = writable('');
+    export const freeNFTstatus = writable('');
 
+    let account;
     const openMetamask = async () =>  {
-       let account;
         await ethereum.request({ method: 'eth_requestAccounts'}).then(accounts => {
             console.log(accounts)
            account = accounts[0];
@@ -28,13 +28,16 @@
     const freeNFTcheck = async () => {
         const url = `https://express-api.codeboxxtest.xyz/NFT/gift/`;
         // const wallet_address = `0x1E90cf48F11Dd52802eC7D1AF7082122A6a59312`;
-        const resp = await fetch(`${url}${$wallet_address}`);
+        await ethereum.request({ method: 'eth_requestAccounts'}).then(accounts => {
+            console.log(accounts)
+            account = accounts[0];})
+        const resp = await fetch(`${url}${account}`);
         const data = await resp.json();
         console.log("Free NFT? ",data)
-
-        return {
-            data
-        }
+        
+        freeNFTstatus.set(data)
+        console.log("Free NFT STATUS :",($freeNFTstatus))
+       
     }
 
     
@@ -45,19 +48,21 @@
 <h1 class="text-4xl text-center my-8 uppercase">NFT Minting Page</h1>
 
 <div class="flex flex-col justify-center">
-<button on:click={openMetamask} id="connect-button" class="p-2 my-2 text-lg bg-blue-300 text-center rounded-lg shadow-sm hover:shadow-md">Connect Metamask</button>
-<button  on:click={freeNFTcheck} id="freenft-button" class="p-2 my-2 text-lg bg-red-400 text-center rounded-lg shadow-sm hover:shadow-md">Get Free NFT</button>
+<button on:click={openMetamask} id="connect-button" class="p-2 my-2 text-lg bg-blue-300 text-center rounded-lg shadow-sm hover:shadow-lg">Connect Metamask</button>
+<button  on:click={freeNFTcheck} id="freenft-button" class="p-2 my-2 text-lg bg-red-400 text-center rounded-lg shadow-sm hover:shadow-lg">Get Free NFT</button>
 </div>
 
 
-
-{#if {freeNFTcheck} === true}
-
-<p>You are eligible for a free NFT</p>
-
-{:else}
-
-<p>You are not eligible for free NFT, please buy an NFT with ROCKET tocken.</p>
-
-{/if}
+<div class="flex justify-center my-8 uppercase text-center">
+    {#if {freeNFTstatus} == true}
+    <div>
+         <p>You are eligible for a free NFT</p>
+    </div>
+    {:else}
+    <div class="">
+        <p>You are not eligible for free NFT, please buy an NFT with ROCKET token.</p>
+       <button  id="buynft-button" class="p-2 my-2 text-lg bg-red-400 text-center rounded-lg shadow-sm hover:shadow-lg">Buy NFT</button>
+    </div>
+    {/if}
+</div>
 
